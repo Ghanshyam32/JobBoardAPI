80fcd6420e602c1d6971fff37c278f477bac9da8
Ghanshyam32 <ghanshyammishra3205615@gmail.com>
2024-03-16 00:34:23 +0530

    intial commit

---
+++ src/main/java/com/example/Jobdone/JobDoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jobdone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobDoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Jobdone/JobDoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jobdone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobDoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
